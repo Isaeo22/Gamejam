2cbd4f4f2332b689199530bf4cb7867dfa40a6a4
Isaeo22 <isaeo22@gmail.com>
2022-10-25 00:01:34 +0200

    sdvlksd<

--- Assets/Scripts/Gusano.cs
+++ Assets/Scripts/Gusano.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gusano : MonoBehaviour
 {
     SpriteRenderer srGusi;
     Animator animGus;
+  
+
     // Start is called before the first frame update
     void Start()
     {
         animGus = GetComponent<Animator>();
+      
     }
 
     // Update is called once per frame
@@ -24,7 +28,7 @@ public class Gusano : MonoBehaviour
         if (other.gameObject.tag == "Rata")
         {
             animGus.SetBool("RataCerca", true);
-            
+          
         }
 
     }
